ee0277c3211cbbd94445c1c3cb1746fa3e88fda9
Patniss <vanina.lancelle@gmail.com>
2026-01-22 10:57:12 +0100

    location reload sur index source

--- js/guard.js
+++ js/guard.js
@@ -16,6 +16,6 @@ const logoutBtn = document.getElementById("logout");
 if (logoutBtn) {
   logoutBtn.addEventListener("click", async () => {
     await supabase.auth.signOut();
-    window.location.href = "index.html";
+    window.location.href = "./index.html";
   });
 }
