e9aadc75848a002a367bc9604f132dfa2d542f6c
Patniss <vanina.lancelle@gmail.com>
2026-02-08 17:44:54 +0100

    update nav.js error 404

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@ export async function loadNav() {
   const navContainer = document.getElementById("nav-container");
   if (!navContainer) return;
 
-  const response = await fetch("/nav.html");
+  const response = await fetch("/Patniss/Elina/nav.html");
   const html = await response.text();
   navContainer.innerHTML = html;
 }
