c9c6afe1824e0a03d1d73f7e32571075770b7052
Patniss <vanina.lancelle@gmail.com>
2026-02-10 16:39:01 +0100

    correction complete.html + début list seen

--- js/list.js
+++ js/list.js
@@ -39,10 +39,6 @@ export async function loadIncompleteMovies() {
     pSubtitle.textContent = movie.year;
     const divTags = document.createElement("div");
     divTags.classList.add("is-flex-direction-row");
-    divTags.classList.add("is-justify-content-space-around");
-    const tagSeen = document.createElement("a");
-    tagSeen.classList.add("tag");
-    tagSeen.textContent = "Non vu";
     const completeBtn = document.createElement("a");
     completeBtn.classList.add("tag");
     completeBtn.textContent = "Compléter";
@@ -63,5 +59,58 @@ export async function loadIncompleteMovies() {
 }
 
 export async function loadSeenMovies() {
+  const {
+      data: { session },
+    } = await supabase.auth.getSession();
   
+    if (!session) {
+      window.location.href = "/index.html";
+      return;
+    }
+  
+    const userId = session.user.id;
+
+    const { data, error } = await supabase
+      .from("users_movies")
+      .select("*")
+      .eq("user_id", userId)
+    
+    if (error) {
+      console.error(error);
+      incompleteContainer.textContent = "Erreur lors du chargement des films.";
+      return;
+    }
+
+    data.forEach((movie) => {
+      const column = document.createElement("div");
+      column.classList.add("column");
+      column.classList.add("is-one-quarter");
+      const card = document.createElement("div");
+      card.classList.add("card");
+      const cardContent = document.createElement("div");
+      cardContent.classList.add("card-content");
+      const pTitle = document.createElement("p");
+      pTitle.classList.add("title");
+      pTitle.classList.add("is-5");
+      pTitle.textContent = movie.title;
+      const pSubtitle = document.createElement("p");
+      pSubtitle.classList.add("subtitle");
+      pSubtitle.classList.add("is-6");
+      pSubtitle.textContent = movie.year;
+      const divTags = document.createElement("div");
+      divTags.classList.add("is-flex-direction-row");
+      const completeBtn = document.createElement("a");
+      completeBtn.classList.add("tag");
+      completeBtn.textContent = "Compléter";
+      completeBtn.href = `/Elina/movies/complete.html?id=${movie.id}`;
+
+      divTags.appendChild(tagSeen);
+      divTags.appendChild(completeBtn);
+      cardContent.appendChild(pTitle);
+      cardContent.appendChild(pSubtitle);
+      cardContent.appendChild(divTags);
+      card.appendChild(cardContent);
+      column.appendChild(card);
+    })
+
 }
