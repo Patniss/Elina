462e51d1ea834efeeb9ad4139481db2ac34a1adb
Patniss <vanina.lancelle@gmail.com>
2026-02-11 20:14:07 +0100

    settings ajouter pseudo

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,24 @@
+import { supabase } from "./supabase.js";
+
+const {
+data: { session },
+} = await supabase.auth.getSession();
+
+if (!session) {
+window.location.href = "/index.html";
+return;
+}
+
+const userId = session.user.id;
+const pseudo = session.pseudo;
+const colorTheme = session.theme_color;
+
+const currentPseudo = document.getElementById("current-pseudo");
 const mainColors = document.getElementById("main-colors");
 const saveSettings = document.getElementById("save-settings");
+
+currentPseudo.textContent = pseudo;
+mainColors.value = colorTheme;
 
 mainColors.addEventListener("change", () => {
     console.log("Couleur changée");
