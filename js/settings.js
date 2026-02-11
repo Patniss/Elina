6d68786f2be965479f7e2b6e111e74783309cfba
Patniss <vanina.lancelle@gmail.com>
2026-02-11 21:10:04 +0100

    add loadProfile() settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,34 @@
+import { supabase } from "./supabase.js";
+
+async function loadProfile() {
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    window.location.href = "/index.html";
+    return;
+  }
+
+  const userId = session.user.id;
+
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("pseudo")
+    .eq("id", userId)
+    .single();
+
+  if (error) {
+    console.error("Erreur profil :", error.message);
+    return;
+  }
+
+  const pseudo = data?.pseudo || "Utilisateur";
+  document.getElementById("welcome").textContent = `Bienvenue ${pseudo}`;
+}
+
+const session = loadProfile();
+
 const userId = session.user.id;
 const pseudo = session.pseudo;
 const colorTheme = session.theme_color;
