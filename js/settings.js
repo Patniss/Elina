7d6f95af3aa9c19086a5dcd152ecdfe90a50f211
Patniss <vanina.lancelle@gmail.com>
2026-02-11 22:17:19 +0100

    updating pseudo

--- js/settings.js
+++ js/settings.js
@@ -38,6 +38,7 @@ const settingMovies = document.getElementById("setting-movies");
 const settingShows = document.getElementById("setting-shows");
 const settingDramas = document.getElementById("setting-dramas");
 const settingBooks = document.getElementById("setting-books");
+const settingPseudo = document.getElementById("setting-pseudo");
 
 const currentPseudo = document.getElementById("current-pseudo");
 const mainColors = document.getElementById("main-colors");
@@ -57,3 +58,20 @@ mainColors.addEventListener("change", () => {
     console.log("Couleur changée");
     saveSettings.style.backgroundColor = mainColors.value;
 });
+
+saveSettings.addEventListener("click", () => {
+  if (settingPseudo) {
+    async function updatePseudo() {
+      const { data, error } = await supabase
+          .from("profiles")
+          .update({ pseudo: settingPseudo })
+          .eq("user_id", userId)
+        if (error) {
+          console.error('Erreur lors de la mise à jour :', error)
+        } else {
+          console.log('Profil mis à jour :', data)
+        }
+    }
+    updatePseudo();
+  }
+})
