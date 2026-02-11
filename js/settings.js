0b1780bffe4aea687fbc029988386f38bf627189
Patniss <vanina.lancelle@gmail.com>
2026-02-11 20:26:33 +0100

    settings coche auto

--- js/settings.js
+++ js/settings.js
@@ -12,13 +12,30 @@ return;
 const userId = session.user.id;
 const pseudo = session.pseudo;
 const colorTheme = session.theme_color;
+const modeUser = session.mode;
+const userMovies = session.movies;
+const userShows = session.shows;
+const userDramas = session.dramas;
+const userBooks = session.books;
+
+const settingMovies = getElementById("setting-movies");
+const settingShows = getElementById("setting-shows");
+const settingDramas = getElementById("setting-dramas");
+const settingBooks = getElementById("setting-books");
 
 const currentPseudo = document.getElementById("current-pseudo");
 const mainColors = document.getElementById("main-colors");
+const mode = document.getElementById("mode");
 const saveSettings = document.getElementById("save-settings");
+
+settingMovies.value = userMovies;
+settingShows.value = userShows;
+settingDramas.value = userDramas;
+settingBooks.value = userBooks;
 
 currentPseudo.textContent = pseudo;
 mainColors.value = colorTheme;
+mode.value = modeUser;
 
 mainColors.addEventListener("change", () => {
     console.log("Couleur changée");
