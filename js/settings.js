710b6352c241f131baa7de524eff072ebbf6fd35
Patniss <vanina.lancelle@gmail.com>
2026-02-11 20:02:49 +0100

    test console color change

--- js/settings.js
+++ js/settings.js
@@ -2,5 +2,6 @@ const mainColors = document.getElementById("main-colors");
 const saveSettings = document.getElementById("save-settings");
 
 mainColors.addEventListener("change", () => {
+    console.log("Couleur changée");
     saveSettings.style.backgroundColor = mainColors.value;
 });
