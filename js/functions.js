856055320cbee76b6f6a09edf93c1e41edff65e2
Patniss <vanina.lancelle@gmail.com>
2026-02-24 10:43:21 +0100

    restructuration

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 import { supabase } from "/Elina/js/core/supabase.js";
-import { loadProfile } from "/Elina/js/core/dashboard.js";
+import { loadProfile } from "/Elina/js/modules/dahsboard/dashboard.js";
 
 export function calculateAge(startDate, endDate = new Date()) {
     const start = new Date(startDate);
