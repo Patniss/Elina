06ecb47567d0c1e1975cefac5a8665d30005a3c4
Patniss <vanina.lancelle@gmail.com>
2026-02-25 17:11:17 +0100

    content.js

--- js/modules/movies/movies.actions.js
+++ js/modules/movies/movies.actions.js
@@ -1,14 +1,11 @@
 import { getUserId } from "/Elina/js/services/profiles.service.js";
-import { handleButtonState, changeModeButton } from "/Elina/js/ui/button.js";
+import { handleButtonState } from "/Elina/js/ui/button.js";
 import { addUserMovie, removeUserMovie, updateSeenUserMovie } from "/Elina/js/services/usersMovies.service.js";
 
 export async function handleMovieAction(action, mode, button, typeButton, showButtons = [], hideButtons = [], div) {
     handleButtonState(button, "loading");
 
-    try {
-        await action();
-
-    } catch (error) {
+    try { await action(); } catch (error) {
         console.error(error);
         setTimeout(() => {
             handleButtonState(button, "error");
@@ -33,20 +30,20 @@ export async function handleMovieAction(action, mode, button, typeButton, showBu
 
 export async function addMovie(uuid, mode, button, showButtons = [], hideButtons = [], div) {
     const userId = await getUserId();
-    await handleMovieAction(addUserMovie(userId, uuid), mode, button, "add", showButtons, hideButtons, div);
+    await handleMovieAction(() => addUserMovie(userId, uuid), mode, button, "add", showButtons, hideButtons, div);
 }
 
 export async function deleteMovie(uuid, mode, button, showButtons = [], hideButtons = [], div) {
     const userId = await getUserId();
-    await handleMovieAction(removeUserMovie(userId, uuid), mode, button, "delete", showButtons, hideButtons, div);
+    await handleMovieAction(() => removeUserMovie(userId, uuid), mode, button, "delete", showButtons, hideButtons, div);
 }
 
 export async function toseeMovie(uuid, mode, button, showButtons = [], hideButtons = [], div) {
     const userId = await getUserId();
-    await handleMovieAction(updateSeenUserMovie(userId, uuid, false), mode, button, "tosee", showButtons, hideButtons, div)
+    await handleMovieAction(() => updateSeenUserMovie(userId, uuid, false), mode, button, "tosee", showButtons, hideButtons, div)
 }
 
 export async function seenMovie(uuid, mode, button, showButtons = [], hideButtons = [], div) {
     const userId = await getUserId();
-    await handleMovieAction(updateSeenUserMovie(userId, uuid, false), mode, button, "seen", showButtons, hideButtons, div)
+    await handleMovieAction(() => updateSeenUserMovie(userId, uuid, false), mode, button, "seen", showButtons, hideButtons, div)
 }
