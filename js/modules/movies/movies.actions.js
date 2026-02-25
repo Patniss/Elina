ed6df62ae4729dae929a3b31ec9fe624657b16f7
Patniss <vanina.lancelle@gmail.com>
2026-02-25 15:44:53 +0100

    content

--- js/modules/movies/movies.actions.js
+++ js/modules/movies/movies.actions.js
@@ -3,50 +3,56 @@ import { handleButtonState, changeModeButton } from "/Elina/js/ui/button.js";
 import { debounce } from "/Elina/js/utils/debounce.js";
 import { addUserMovie, removeUserMovie, updateSeenUserMovie } from "/Elina/js/services/usersMovies.service.js";
 
-export async function handleMovieAction(action, uuid, mode, button, typeButton, showButtons = [], hideButtons = [], div, seen) {
-    const userId = await getUserId();
-
-    console.log("actions.js handleMovieAction() movie_id: ", uuid);
-    console.log("actions.js handleMovieAction() userId", userId);
-
+export async function handleMovieAction(action, mode, button, typeButton, showButtons = [], hideButtons = [], div) {
     handleButtonState(button, "loading");
 
     try {
-        const actionMovie = await action(userId, uuid, seen);
+        const actionMovie = await action();
 
         if (actionMovie.error) {
-            debounce(() => handleButtonState(button, "error"), 500)();
+            setTimeout(() => {
+                handleButtonState(button, "error");
+            }, 500);
             return;
         }
     } catch (error) {
-        debounce(() => handleButtonState(button, "error"), 500)();
+        setTimeout(() => {
+            handleButtonState(button, "error");
+        }, 500);
         return;
     }
 
     if (mode === "adding") {
-        debounce(() => changeModeButton(button, typeButton), 500)();
-        div.innerHTML = "";
-        div.append(showButtons);
+        setTimeout(() => {
+            handleButtonState(button, typeButton);
+            div.innerHTML = "";
+            div.append(showButtons);
+        }, 500);
     } else if (mode === "hidden") {
-        debounce(() => changeModeButton(button, typeButton), 500)();
-        hideButtons.classList.add("is-hidden");
-        showButtons.classList.remove("is-hidden");
+        setTimeout(() => {
+            handleButtonState(button, typeButton);
+            hideButtons.classList.add("is-hidden");
+            showButtons.classList.remove("is-hidden");
+        }, 500);
     }
 }
 
 export async function addMovie(uuid, mode, button, showButtons = [], hideButtons = [], div) {
     const userId = await getUserId();
-    await handleMovieAction(addUserMovie(userId, uuid), uuid, mode, button, "add", showButtons, hideButtons, div);
+    await handleMovieAction(addUserMovie(userId, uuid), mode, button, "add", showButtons, hideButtons, div);
 }
 
 export async function deleteMovie(uuid, mode, button, showButtons = [], hideButtons = [], div) {
-    await handleMovieAction(removeUserMovie, uuid, mode, button, "delete", showButtons, hideButtons, div);
+    const userId = await getUserId();
+    await handleMovieAction(removeUserMovie(userId, uuid), mode, button, "delete", showButtons, hideButtons, div);
 }
 
 export async function toseeMovie(uuid, mode, button, showButtons = [], hideButtons = [], div) {
-    await handleMovieAction(updateSeenUserMovie, uuid, mode, button, "tosee", showButtons, hideButtons, div, false)
+    const userId = await getUserId();
+    await handleMovieAction(updateSeenUserMovie(userId, uuid, false), mode, button, "tosee", showButtons, hideButtons, div)
 }
 
 export async function seenMovie(uuid, mode, button, showButtons = [], hideButtons = [], div) {
-    await handleMovieAction(updateSeenUserMovie, uuid, mode, button, "seen", showButtons, hideButtons, div, true)
+    const userId = await getUserId();
+    await handleMovieAction(updateSeenUserMovie(userId, uuid, false), mode, button, "seen", showButtons, hideButtons, div)
 }
