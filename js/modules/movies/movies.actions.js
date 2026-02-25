dfb8199823246903f13e97930be5a372795736e9
Patniss <vanina.lancelle@gmail.com>
2026-02-25 17:47:32 +0100

    Update movies.actions.js

--- js/modules/movies/movies.actions.js
+++ js/modules/movies/movies.actions.js
@@ -1,5 +1,5 @@
 import { getUserId } from "/Elina/js/services/profiles.service.js";
-import { handleButtonState } from "/Elina/js/ui/button.js";
+import { changeModeButton } from "/Elina/js/ui/button.js";
 import { addUserMovie, removeUserMovie, updateSeenUserMovie } from "/Elina/js/services/usersMovies.service.js";
 
 export async function handleMovieAction(action, mode, button, typeButton, showButtons = [], hideButtons = [], div) {
@@ -15,13 +15,13 @@ export async function handleMovieAction(action, mode, button, typeButton, showBu
 
     if (mode === "adding") {
         setTimeout(() => {
-            handleButtonState(button, typeButton);
+            changeModeButton(button, typeButton);
             div.innerHTML = "";
             div.append(showButtons);
         }, 500);
     } else if (mode === "hidden") {
         setTimeout(() => {
-            handleButtonState(button, typeButton);
+            changeModeButton(button, typeButton);
             hideButtons.classList.add("is-hidden");
             showButtons.classList.remove("is-hidden");
         }, 500);
