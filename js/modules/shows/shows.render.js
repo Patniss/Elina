924c6fc7899d623b9529cc26ce57e556dcd5e4d7
Patniss <vanina.lancelle@gmail.com>
2026-03-01 09:57:08 +0100

    show retirer pagination pour test

--- js/modules/shows/shows.render.js
+++ js/modules/shows/shows.render.js
@@ -1,11 +1,8 @@
 import { render } from "/Elina/js/ui/render.js";
 import { createShowCard } from "/Elina/js/modules/shows/shows.ui.js";
 import { showsStore } from "/Elina/js/data/shows.store.js";
-import { renderPaginationAll } from "/Elina/js/modules/shows/shows.pagination.js";
 
 export async function renderAllShows() {
     const container = document.getElementById("list-all-shows");
     render(container, showsStore, "all", "shows", createShowCard);
-
-    renderPaginationAll();
 }
