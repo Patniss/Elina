631891548d4a8b5fb8848076e651ee0357c2e1bd
Patniss <vanina.lancelle@gmail.com>
2026-03-01 09:38:58 +0100

    Update movies.pagination.js

--- js/modules/movies/movies.pagination.js
+++ js/modules/movies/movies.pagination.js
@@ -3,12 +3,11 @@ import { renderPagination } from "/Elina/js/ui/pagination.js";
 import { renderAllMovies } from "/Elina/js/modules/movies/movies.render.js";
 
 export function renderPaginationAll() {
-    renderPagination({
-        container: "pagination-list",
-        currentPage: moviesStore.currentPage.all,
-        totalItems: moviesStore.movies.all.length,
-        pageSize: moviesStore.pageSize,
-        setCurrentPage: (page) => { moviesStore.setCurrentPage("all", page); },
-        onPageChange: () => { renderAllMovies(); }
-    })
+    renderPagination(
+        "pagination-list",
+        moviesStore.currentPage.all,
+        moviesStore.movies.all.length,
+        moviesStore.pageSize,
+        (page) => { moviesStore.setCurrentPage("all", page); },
+        () => { renderAllMovies(); })
 }
