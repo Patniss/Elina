c729f3091de80edf788423fe0321cf43280d9a73
Patniss <vanina.lancelle@gmail.com>
2026-03-01 12:15:09 +0100

    test doublons render

--- js/modules/movies/movies.render.js
+++ js/modules/movies/movies.render.js
@@ -4,6 +4,7 @@ import { moviesStore } from "/Elina/js/data/movies.store.js";
 import { renderPaginationAll } from "/Elina/js/modules/movies/movies.pagination.js";
 
 export async function renderAllMovies() {
+    console.log("movies render appelé");
     const container = document.getElementById("list-all-movies");
     render(container, moviesStore, "all", "movies", createMovieCard);
 
