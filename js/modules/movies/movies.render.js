99453cd17dd2b470a88428d375a2ae29856f3960
Patniss <vanina.lancelle@gmail.com>
2026-03-02 12:00:52 +0100

    créatation list.html + ajout renderMyMovies

--- js/modules/movies/movies.render.js
+++ js/modules/movies/movies.render.js
@@ -4,9 +4,12 @@ import { moviesStore } from "/Elina/js/data/movies.store.js";
 import { renderPaginationAll } from "/Elina/js/modules/movies/movies.pagination.js";
 
 export async function renderAllMovies() {
-    console.log("movies render appelé");
     const container = document.getElementById("list-all-movies");
     render(container, moviesStore, "all", "movies", createMovieCard);
 
     renderPaginationAll();
+}
+
+export async function renderMyMovies() {
+    // construire la fonction
 }
