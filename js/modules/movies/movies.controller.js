5ce2daa100cc175af4cad38281d7ae84700ca951
Patniss <vanina.lancelle@gmail.com>
2026-03-02 13:17:22 +0100

    Update movies.controller.js

--- js/modules/movies/movies.controller.js
+++ js/modules/movies/movies.controller.js
@@ -1,56 +1,72 @@
-// movies.controller.js
-import { initResearchMovie } from "/Elina/js/modules/movies/movies.search.js";
+import { initResearchMovie } from "/Elina/js/movies/movies.search";
 import { moviesStore } from "/Elina/js/data/movies.store.js";
-import { loadAllMovies, loadToseeMovies, loadSeenMovies } from "/Elina/js/modules/movies/movies.load.js";
-import { renderAllMovies, renderMyMovies } from "/Elina/js/modules/movies/movies.render.js";
-import { initToggleSection } from "/Elina/js/ui/toggles.js";
+import { loadAllMovies } from "/Elina/js/modules/movies/movies.load.js";
+import { renderAllMovies } from "/Elina/js/modules/movies/movies.render.js";
+import { initToggleSection } from "/Elina/js/utils/toggles.js";
 
 let unsubscribe;
 
-export async function initMoviesAll() {
-  // unsubscribe précédent si existant
-  unsubscribe?.();
-  unsubscribe = moviesStore.subscribe(list => {
-    if (list === "all") renderAllMovies();
-  });
+export async function initMovies() {
+    unsubscribe?.();
 
-  initResearchMovie();
+    if (list === "tosee" || list === "seen") {
+        initToggleSection({
+            toggleId: "toggle-tosee",
+            contentId: "div-tosee-movies",
+            arrowId: "arrow-tosee"
+        });
 
-  const movies = await loadAllMovies(moviesStore.sortField, moviesStore.sortAsc, moviesStore.genreFilter);
-  moviesStore.setMoviesAndPage("all", movies, 1);
+        initToggleSection({
+            toggleId: "toggle-seen",
+            contentId: "div-seen-movies",
+            arrowId: "arrow-seen"
+        });
+    }
+
+    unsubscribe = moviesStore.subscribe((list) => {
+        if (list === "all") renderAllMovies();
+        if (list === "tosee" || list === "seen") renderMyMovies();
+    });
+
+    initResearchMovie();
+
+    await refreshMovies(list);
 }
 
-export async function initMoviesMy() {
-  unsubscribe?.();
-  unsubscribe = moviesStore.subscribe(list => {
-    if (list === "tosee" || list === "seen") renderMyMovies();
-  });
+export async function refreshMovies() {
+    let movies;
+    switch (list) {
+        case "all":
+            movies = await loadAllMovies(moviesStore.sortField, moviesStore.sortAsc, moviesStore.genreFilter);
+            break;
+    
+        case "tosee":
+            movies = await loadToseeMovies(moviesStore.sortField, moviesStore.sortAsc, moviesStore.genreFilter);
+            break;
+        
+        case "seen":
+            movies = await loadSeenMovies(moviesStore.sortField, moviesStore.sortAsc, moviesStore.genreFilter);
+            break;
+    }
 
-  initResearchMovie();
-
-  // init toggles (une seule fois par page)
-  initToggleSection({
-    toggleId: "toggle-tosee",
-    contentId: "div-tosee-movies",
-    arrowId: "arrow-tosee"
-  });
-  initToggleSection({
-    toggleId: "toggle-seen",
-    contentId: "div-seen-movies",
-    arrowId: "arrow-seen"
-  });
-
-  // fetch séparé
-  const tosee = await loadToseeMovies(moviesStore.sortField, moviesStore.sortAsc, moviesStore.genreFilter);
-  const seen = await loadSeenMovies(moviesStore.sortField, moviesStore.sortAsc, moviesStore.genreFilter);
-
-  moviesStore.setState({
-    movies: { ...moviesStore.movies, tosee, seen },
-    currentPage: { ...moviesStore.currentPage, tosee: 1, seen: 1 }
-  });
+    if (list === "tosee" || list === "seen") {
+        moviesStore.setState({
+            movies: {
+                ...moviesStore.movies,
+                tosee,
+                seen
+            },
+            currentPage: {
+                ...moviesStore.currentPage,
+                tosee: 1,
+                seen: 1
+            }
+        });
+    }
+    
+    moviesStore.setMoviesAndPage(list, movies, 1);
 }
 
-// changement de page pour n'importe quelle liste
 export async function changePage(list, page) {
-  moviesStore.setCurrentPage(list, page);
+    moviesStore.setCurrentPage(list, page);
 }
