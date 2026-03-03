86990d31331c83784f4afeab7d10a059cc6eb78f
Patniss <vanina.lancelle@gmail.com>
2026-03-03 09:32:09 +0100

    remise controller base

--- js/modules/movies/movies.controller.js
+++ js/modules/movies/movies.controller.js
@@ -2,71 +2,22 @@ import { initResearchMovie } from "/Elina/js/modules/movies/movies.search.js";
 import { moviesStore } from "/Elina/js/data/movies.store.js";
 import { loadAllMovies } from "/Elina/js/modules/movies/movies.load.js";
 import { renderAllMovies } from "/Elina/js/modules/movies/movies.render.js";
-import { initToggleSection } from "/Elina/js/ui/toggles.js";
-
-let unsubscribe;
 
 export async function initMovies() {
-    unsubscribe?.();
-
-    if (list === "tosee" || list === "seen") {
-        initToggleSection({
-            toggleId: "toggle-tosee",
-            contentId: "div-tosee-movies",
-            arrowId: "arrow-tosee"
-        });
-
-        initToggleSection({
-            toggleId: "toggle-seen",
-            contentId: "div-seen-movies",
-            arrowId: "arrow-seen"
-        });
-    }
-
-    unsubscribe = moviesStore.subscribe((list) => {
-        if (list === "all") renderAllMovies();
-        if (list === "tosee" || list === "seen") renderMyMovies();
+    moviesStore.subscribe(() => {
+        renderAllMovies();
     });
 
     initResearchMovie();
 
-    await refreshMovies(list);
+    await refreshMovies();
 }
 
 export async function refreshMovies() {
-    let movies;
-    switch (list) {
-        case "all":
-            movies = await loadAllMovies(moviesStore.sortField, moviesStore.sortAsc, moviesStore.genreFilter);
-            break;
-    
-        case "tosee":
-            movies = await loadToseeMovies(moviesStore.sortField, moviesStore.sortAsc, moviesStore.genreFilter);
-            break;
-        
-        case "seen":
-            movies = await loadSeenMovies(moviesStore.sortField, moviesStore.sortAsc, moviesStore.genreFilter);
-            break;
-    }
-
-    if (list === "tosee" || list === "seen") {
-        moviesStore.setState({
-            movies: {
-                ...moviesStore.movies,
-                tosee,
-                seen
-            },
-            currentPage: {
-                ...moviesStore.currentPage,
-                tosee: 1,
-                seen: 1
-            }
-        });
-    }
-    
-    moviesStore.setMoviesAndPage(list, movies, 1);
+    const movies = await loadAllMovies(moviesStore.sortField, moviesStore.sortAsc, moviesStore.genreFilter);
+    moviesStore.setMoviesAndPage("all", movies, 1);
 }
 
-export async function changePage(list, page) {
-    moviesStore.setCurrentPage(list, page);
+export async function changePage(page) {
+    moviesStore.setCurrentPage("all", page);
 }
