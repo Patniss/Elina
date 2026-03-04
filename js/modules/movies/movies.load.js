04fd2fd5347977cf1b8b4e3a3cf080b0d25f162f
Patniss <vanina.lancelle@gmail.com>
2026-03-04 10:36:51 +0100

    Update movies.load.js

--- js/modules/movies/movies.load.js
+++ js/modules/movies/movies.load.js
@@ -1,5 +1,5 @@
 import { supabase } from "/Elina/js/core/supabase.js";
-import { sortMovies, filterMovies } from "/Elina/js/modules/movies/movies.layout.js";
+import { sortMovies, filterMovies, sortUsersMovies } from "/Elina/js/modules/movies/movies.layout.js";
 import { querySupabase } from "/Elina/js/services/service.js";
 import { mapMoviesWithStatus } from "/Elina/js/modules/usersMovies/usersMovies.status.js";
 import { getUserId } from "/Elina/js/services/profiles.service.js";
@@ -20,7 +20,7 @@ export async function loadMyMovies(field, asc, filter) {
     const userId = await getUserId();
     let query = supabase.from("users_movies").select("*, movies(*)").eq("user_id", userId);
     
-    query = sortMovies(query, field, asc);
+    query = sortUsersMovies(query, field, asc);
     query = filterMovies(query, filter);
 
     const movies = await querySupabase(query);
