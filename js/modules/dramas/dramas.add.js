1b8a3727e9ec70e0d6ca1f3ef19f4973e05ad353
Patniss <vanina.lancelle@gmail.com>
2026-03-16 15:39:37 +0100

    add drama

--- js/modules/dramas/dramas.add.js
+++ js/modules/dramas/dramas.add.js
@@ -1,3 +1,13 @@
+import { genres } from "/Elina/js/data/genres.js";
+import { initGenres } from "/Elina/js/ui/select.js";
+
 export async function addDrama() {
-    
+    const titleInput = document.getElementById("drama-title");
+    const nbEpisodesInput = document.getElementById("drama-nb-episodes");
+    const averageTimeInput = document.getElementById("drama-average-time");
+    const genresInput = document.getElementById("drama-genres");
+    const tagsInput = document.getElementById("drama-tags");
+    const posterInput = document.getElementById("drama-poster");
+
+    initGenres(genresInput, genres);
 }
