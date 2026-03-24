dc41707de365e14280c7ca5e092eb3c557e5cbf8
Patniss <vanina.lancelle@gmail.com>
2026-03-24 12:22:04 +0100

    calendar

---
+++ js/modules/calendar/calendar.js
@@ -0,0 +1,63 @@
+export function displayCalendar() {
+    const yearDisplay = document.getElementById("year");
+    const januaryDisplay = document.getElementById("january");
+    const februaryDisplay = document.getElementById("february");
+    const marchDisplay = document.getElementById("march");
+    const aprilDisplay = document.getElementById("april");
+    const mayDisplay = document.getElementById("may");
+    const juneDisplay = document.getElementById("june");
+    const julyDisplay = document.getElementById("july");
+    const augustDisplay = document.getElementById("august");
+    const septemberDisplay = document.getElementById("september");
+    const octoberDisplay = document.getElementById("october");
+    const novemberDisplay = document.getElementById("november");
+    const decemberDisplay = document.getElementById("december");
+
+    const nowDate = new Date();
+    const nowYear = new Date().getFullYear();
+
+    function createWeek(table, start, max) {
+        const tr = document.createElement("tr");
+        for (let w = 1; w <= 7; w++) {
+            const td = document.createElement("td");
+            td.textContent = start > 0 ? start <= max ? start : " " : " ";
+            start++;
+            tr.appendChild(td);
+        }
+
+        table.appendChild(tr);
+    }
+
+    let dayOfWeek;
+
+    function createMonth(table, nbYear, nbMonth) {
+        const firstMonth = new Date(nbYear, nbMonth, 1);
+        const lastMonth = new Date(nbYear, (nbMonth + 1), 0);
+
+        dayOfWeek = firstMonth.getDay();
+        dayOfWeek = dayOfWeek === 0 ? 7 : dayOfWeek;
+
+        let start = dayOfWeek === 0 ? 1 : (1 - dayOfWeek);
+        const lastDayOfMonth = lastMonth.getDate();
+
+        while (start <= lastDayOfMonth) {
+            createWeek(table, start, lastDayOfMonth);
+            start += 7;
+        }
+    }
+
+    createMonth(januaryDisplay, nowYear, 0);
+    createMonth(februaryDisplay, nowYear, 1);
+    createMonth(marchDisplay, nowYear, 2);
+    createMonth(aprilDisplay, nowYear, 3);
+    createMonth(mayDisplay, nowYear, 4);
+    createMonth(juneDisplay, nowYear, 5);
+    createMonth(julyDisplay, nowYear, 6);
+    createMonth(augustDisplay, nowYear, 7);
+    createMonth(septemberDisplay, nowYear, 8);
+    createMonth(octoberDisplay, nowYear, 9);
+    createMonth(novemberDisplay, nowYear, 10);
+    createMonth(decemberDisplay, nowYear, 11);
+
+    yearDisplay.textContent = nowYear;
+}
