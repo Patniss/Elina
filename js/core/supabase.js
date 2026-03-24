3ce4b08a350dbbf24e93253e70cfae218735839e
Patniss <vanina.lancelle@gmail.com>
2026-03-24 12:53:33 +0100

    supabase local

--- js/core/supabase.js
+++ js/core/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const supabaseUrl = "https://clffgsknhzqlywvcimcj.supabase.co";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
