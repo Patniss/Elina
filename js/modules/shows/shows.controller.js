e1b9f5526a1220d9b823df439270c53f2f57e7db
Patniss <vanina.lancelle@gmail.com>
2026-03-26 11:53:14 +0100

    current shows

--- js/modules/shows/shows.controller.js
+++ js/modules/shows/shows.controller.js
@@ -7,72 +7,19 @@ export async function changePage(page) {
     showsStore.setCurrentPage("all", page);
 }
 
-export async function initMyShows() {
+export async function initCurrentShows() {
     showsStore.genreFilter = "";
     showsStore.sortField = "title";
     showsStore.sortAsc = true;
 
-    initToggleSection({
-        toggleId: "toggle-current",
-        contentId: "div-current-shows",
-        arrowId: "arrow-current",
-        hiddenClass: "is-hidden",
-        iconOpen: "fa-chevron-down",
-        iconClosed:"fa-chevron-right"
-    });
+    showsStore.subscribe(() => {
+        renderCurrentShows();
 
-    initToggleSection({
-        toggleId: "toggle-paused",
-        contentId: "div-paused-shows",
-        arrowId: "arrow-paused",
-        hiddenClass: "is-hidden",
-        iconOpen: "fa-chevron-down",
-        iconClosed:"fa-chevron-right"
+        // initResearchShow();
+        // sortFilterShows();
     });
-
-    initToggleSection({
-        toggleId: "toggle-tostart",
-        contentId: "div-tostart-shows",
-        arrowId: "arrow-tostart",
-        hiddenClass: "is-hidden",
-        iconOpen: "fa-chevron-down",
-        iconClosed:"fa-chevron-right"
-    });
-
-    initToggleSection({
-        toggleId: "toggle-finished",
-        contentId: "div-finished-shows",
-        arrowId: "arrow-finished",
-        hiddenClass: "is-hidden",
-        iconOpen: "fa-chevron-down",
-        iconClosed:"fa-chevron-right"
-    });
-
-    initToggleSection({
-        toggleId: "toggle-canceled",
-        contentId: "div-canceled-shows",
-        arrowId: "arrow-canceled",
-        hiddenClass: "is-hidden",
-        iconOpen: "fa-chevron-down",
-        iconClosed:"fa-chevron-right"
-    });
-
-    showsStore.subscribe(async () => {
-        await renderCurrentShows();
-        await renderPausedShows();
-        await renderTostartShows();
-        await renderFinishedShows();
-        await renderCanceledShows();
-    });
-
-    // initResearchCurrentShows();
-    // sortFilterShows();
 
     await refreshCurrentShows();
-    await refreshPausedShows();
-    await refreshTostartShows();
-    await refreshFinishedShows();
-    await refreshCanceledShows();
 }
 
 export async function initShows() {
@@ -90,16 +37,6 @@ export async function initShows() {
     await refreshShows();
 }
 
-export async function refreshCanceledShows() {
-    try {
-        const shows = await loadCanceledShows(showsStore.sortField, showsStore.sortAsc, showsStore.genreFilter);
-        showsStore.setShowsAndPage("canceled", shows, 1);
-    } catch (error) {
-        console.error("Erreur refresh:", error);
-        return;
-    }
-}
-
 export async function refreshCurrentShows() {
     try {
         const shows = await loadCurrentShows(showsStore.sortField, showsStore.sortAsc, showsStore.genreFilter);
@@ -110,37 +47,7 @@ export async function refreshCurrentShows() {
     }
 }
 
-export async function refreshFinishedShows() {
-    try {
-        const shows = await loadFinishedShows(showsStore.sortField, showsStore.sortAsc, showsStore.genreFilter);
-        showsStore.setShowsAndPage("finished", shows, 1);
-    } catch (error) {
-        console.error("Erreur refresh:", error);
-        return;
-    }
-}
-
-export async function refreshPausedShows() {
-    try {
-        const shows = await loadPausedShows(showsStore.sortField, showsStore.sortAsc, showsStore.genreFilter);
-        showsStore.setShowsAndPage("paused", shows, 1);
-    } catch (error) {
-        console.error("Erreur refresh:", error);
-        return;
-    }
-}
-
 export async function refreshShows() {
     const shows = await loadAllShows(showsStore.sortField, showsStore.sortAsc, showsStore.genreFilter);
     showsStore.setShowsAndPage("all", shows, 1);
-}
-
-export async function refreshTostartShows() {
-    try {
-        const shows = await loadTostartShows(showsStore.sortField, showsStore.sortAsc, showsStore.genreFilter);
-        showsStore.setShowsAndPage("tostart", shows, 1);
-    } catch (error) {
-        console.error("Erreur refresh:", error);
-        return;
-    }
 }
