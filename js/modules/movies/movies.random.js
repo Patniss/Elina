ed62f447d72680c43276c0e2ed645013b1582cfd
Patniss <vanina.lancelle@gmail.com>
2026-03-31 09:53:42 +0200

    Update movies.random.js

--- js/modules/movies/movies.random.js
+++ js/modules/movies/movies.random.js
@@ -5,12 +5,21 @@ import { initGenres, initPlaforms } from "/Elina/js/ui/select.js";
 const platforms = ["Netflix", "Disney+", "Amazon Prime"];
 
 export async function getRandomMovie(selectedGenres, selectedPlatforms) {
+    console.log(selectedGenres);
     allToseeMovies = await getToseeMovies();
 }
 
 export async function displayRandomMovie() {
     const selectGenres = document.getElementById("movie-random-genres");
     const selectPlatforms = document.getElementById("movie-random-platforms");
+    const buttonDice = document.getElementById("movie-random-button");
+
     initGenres(selectGenres, genres);
     initPlaforms(selectPlatforms, platforms);
+
+    buttonDice.addEventListener("click", async () => {
+        let selectedGenres = $(selectGenres).val() || [];
+
+        await getRandomMovie(selectedGenres);
+    })
 }
