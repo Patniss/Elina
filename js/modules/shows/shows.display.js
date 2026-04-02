6289e993f8c1de51a24b8dbc9adca9575906e123
Patniss <vanina.lancelle@gmail.com>
2026-04-02 10:45:47 +0200

    Update shows.display.js

--- js/modules/shows/shows.display.js
+++ js/modules/shows/shows.display.js
@@ -1,16 +1,23 @@
 import { getProfileData } from "/Elina/js/services/profilesData.service.js";
+import { getCurrentShows } from "/Elina/js/services/usersShows.service.js";
 import { formatTotalTime } from "/Elina/js/utils/format.js";
 
 export async function displayIndexShows() {
     const showsCurrent = document.getElementById("shows-current");
     const episodesSeen = document.getElementById("episodes-seen");
     const totalTimeSeen = document.getElementById("total-time-seen");
-    const currentShowsTosee = document.getElementById("tosee-current");
+    const currentToseeEpisodes = document.getElementById("tosee-current");
 
     const profileData = await getProfileData();
+    console.log(profileData);
 
     showsCurrent.textContent = profileData.current_shows;
     episodesSeen.textContent = profileData.episodes_seen;
     totalTimeSeen.textContent = formatTotalTime(profileData.time_shows_seen);
-    currentShowsTosee.textContent = profileData.epsiodes_tosee;
+    currentToseeEpisodes.textContent = profileData.epsiodes_tosee;
+
+    const currentShowsDisplay = document.getElementById("current-shows");
+
+    const currentShows = await getCurrentShows;
+    console.log(currentShows);
 }
