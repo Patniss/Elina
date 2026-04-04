042ed26e482e0b537d22024c91bfde5457f5cea4
Patniss <vanina.lancelle@gmail.com>
2026-04-04 15:22:21 +0200

    add people content movies

--- js/modules/movies/movies.complete.js
+++ js/modules/movies/movies.complete.js
@@ -10,4 +10,52 @@ export async function completeMovie() {
     initPeopleSelect(selectPeople, people);
 
     initPeopleSelect2NewTag(selectPeople, "Saisir un nom…");
+
+    selectPeople.on("select2:select", function (e) {
+        const data = e.params.data;
+
+        if (data.newTag) {
+            document.getElementById("div-add-people").classList.remove("is-hidden");
+            document.getElementById("div-choose-people").classList.add("is-hidden");
+
+            const firstnamePeople = document.getElementById("firstname-people");
+            const lastnamePeople = document.getElementById("lastname-people");
+            const birthdatePeople = document.getElementById("birthdate-people");
+            const isDeadPeople = document.getElementById("is-dead-people");
+            const deathdatePeople = document.getElementById("deathdate-people");
+            const nationalitiesPeople = document.getElementById("nationalities-people");
+            const cancelAdd = document.getElementById("btn-cancel-add-people");
+            const btnAddPeople = document.getElementById("btn-add-people");
+
+            firstnamePeople.required = true;
+            lastnamePeople.required = true;
+            birthdatePeople.required = true;
+            nationalitiesPeople.required = true;
+
+            const { firstName, lastName } = parseFullName(data.text);
+            firstnamePeople.value = firstName;
+            lastnamePeople.value = lastName;
+
+            isDeadPeople.addEventListener("change", () => {
+                if (isDeadPeople.checked) {
+                    document.getElementById("div-deathdate-people").classList.remove("is-hidden");
+                    deathdatePeople.required = true;
+                } else {
+                    document.getElementById("div-deathdate-people").classList.add("is-hidden");
+                    deathdatePeople.required = false;
+                }
+            });
+
+            cancelAdd.addEventListener("click", () => {
+                document.getElementById("div-add-people").classList.add("is-hidden");
+                document.getElementById("div-choose-people").classList.remove("is-hidden");
+
+                firstnamePeople.required = false;
+                lastnamePeople.required = false;
+                birthdatePeople.required = false;
+                nationalitiesPeople.required = false;
+                deathdatePeople.required = false;
+            });
+        }
+    })
 }
