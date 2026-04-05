c73eeab1731debac2802928be33d7577e47fb714
Patniss <vanina.lancelle@gmail.com>
2026-04-05 11:33:49 +0200

    finished shows

--- js/modules/shows/shows.controller.js
+++ js/modules/shows/shows.controller.js
@@ -1,6 +1,6 @@
 import { showsStore } from "/Elina/js/data/shows.store.js";
-import { loadAllShows, loadCurrentShows } from "/Elina/js/modules/shows/shows.load.js";
-import { renderAllShows, renderCurrentShows, renderPausedShows, renderTostartShows } from "/Elina/js/modules/shows/shows.render.js";
+import { loadAllShows, loadCurrentShows, loadPausedShows, loadFinishedShows } from "/Elina/js/modules/shows/shows.load.js";
+import { renderAllShows, renderCurrentShows, renderPausedShows, renderTostartShows, renderFinishedShows } from "/Elina/js/modules/shows/shows.render.js";
 
 export async function changePage(page) {
     showsStore.setCurrentPage("all", page);
@@ -21,6 +21,21 @@ export async function initCurrentShows() {
     await refreshCurrentShows();
 }
 
+export async function initFinishedShows() {
+    showsStore.genreFilter = "";
+    showsStore.sortField = "title";
+    showsStore.sortAsc = true;
+
+    showsStore.subscribe(() => {
+        renderFinishedShows();
+
+        // initResearchShow();
+        // sortFilterShows();
+    });
+
+    await refreshFinishedShows();
+}
+
 export async function initPausedShows() {
     showsStore.genreFilter = "";
     showsStore.sortField = "title";
@@ -32,6 +47,8 @@ export async function initPausedShows() {
         // initResearchShow();
         // sortFilterShows();
     });
+
+    await refreshPausedShows();
 }
 
 export async function initShows() {
@@ -66,6 +83,26 @@ export async function refreshCurrentShows() {
     try {
         const shows = await loadCurrentShows(showsStore.sortField, showsStore.sortAsc, showsStore.genreFilter);
         showsStore.setShowsAndPage("current", shows, 1);
+    } catch (error) {
+        console.error("Erreur refresh:", error);
+        return;
+    }
+}
+
+export async function refreshFinishedShows() {
+    try {
+        const shows = await loadFinishedShows(showsStore.sortField, showsStore.sortAsc, showsStore.genreFilter);
+        showsStore.setShowsAndPage("finished", shows, 1);
+    } catch (error) {
+        console.error("Erreur refresh:", error);
+        return;
+    }
+}
+
+export async function refreshPausedShows() {
+    try {
+        const shows = await loadPausedShows(showsStore.sortField, showsStore.sortAsc, showsStore.genreFilter);
+        showsStore.setShowsAndPage("paused", shows, 1);
     } catch (error) {
         console.error("Erreur refresh:", error);
         return;
