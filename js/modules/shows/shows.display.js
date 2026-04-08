3f0cd6f4206015d183e0bd442324ae08ae124110
Patniss <vanina.lancelle@gmail.com>
2026-04-08 15:09:05 +0200

    display index shows

--- js/modules/shows/shows.display.js
+++ js/modules/shows/shows.display.js
@@ -4,16 +4,21 @@ import { getCurrentShows } from "/Elina/js/services/usersShows.service.js";
 import { formatTotalTime } from "/Elina/js/utils/format.js";
 
 export async function displayIndexShows() {
-    const showsCurrent = document.getElementById("shows-current");
-    const episodesSeen = document.getElementById("episodes-seen");
-    const totalTimeSeen = document.getElementById("total-time-seen");
-    const currentToseeEpisodes = document.getElementById("tosee-current");
-
     const profileData = await getProfileData();
 
+    const showsFinished = document.getElementById("shows-finished");
+    showsFinished.textContent = profileData.finished_shows;
+
+    const showsCurrent = document.getElementById("shows-current");
     showsCurrent.textContent = profileData.current_shows;
+
+    const episodesSeen = document.getElementById("episodes-seen");
     episodesSeen.textContent = profileData.episodes_seen;
+
+    const totalTimeSeen = document.getElementById("total-time-seen");
     totalTimeSeen.textContent = formatTotalTime(profileData.time_shows_seen);
+
+    const currentToseeEpisodes = document.getElementById("tosee-current");
     currentToseeEpisodes.textContent = profileData.episodes_tosee;
 
     const currentShows = await getCurrentShows();
